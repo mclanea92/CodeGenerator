a6aad87b29313bec7aadd1c86692ff97d3815f4d
Adam McLane <mclanea92@gmail.com>
2022-03-22 16:12:01 -0400

    Added comments

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+// First function.  This first makes arrays with the specific character elements like below.
 function makePassword() {
     const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
@@ -29,6 +31,9 @@ function makePassword() {
 
     var passwordLength = getPWLength();
 
+// Still part of the first function; This goes though the object properties to determine if they are true or false.
+// This is done with the alert. 
+
     var eleSelect = true;
   if (eleSelect == true) {
     var number = getPass("Numbers")
@@ -36,18 +41,22 @@ function makePassword() {
     var lower = getPass("Lowercase")
     var upper = getPass("Uppercase")
 
+    // if & else are used to go make sure that something is selected yes for when the prompt comes up,
+    // if none are selected, it will display the message "Please select at least 1 type" and returns
+    //to the beginning of the prompts.
       if ((lower) || (upper) || (number) || (special)) {
         eleSelect = false;
       }
     }
-  
+
   else {
       window.alert("Please select at least 1 type.")
     return generatePassword()
   }
 }
 
-
+// new section - this is where if the specific element is considered 'true' it will add it to 
+// the open array 'passwordarray' with characters from their orginal array
 if (number) {
   passwordarray = passwordarray.concat(numbers);
 }
